df592508cdd7e321e5e9051343090d6f44457f93
Ulrich Eck <ulrich.eck@tum.de>
2020-02-20 08:30:45 +0100

    use explicit versions instead of master for tracability

--- conanfile.py
+++ conanfile.py
@@ -4,28 +4,28 @@ import os
 
 class DracoConan(ConanFile):
     name = "draco"
-    version = "0.1"
+    version = "1.3.5"
     generators = "cmake"
     settings = {"os": None, "arch": ["x86_64", "x86"], "compiler": None, "build_type": None}
 
-    options = {"BUILD_SHARED_LIBS": [True, False]}
-    default_options = "BUILD_SHARED_LIBS=True"
+    options = {"shared": [True, False]}
+    default_options = "shared=True"
     exports_sources = "include*", "src*", "cmake*", "CMakeLists.txt"
 
     exports = ["CMakeLists.txt"]
 
-    license="Licensed under the Apache License, Version 2.0"
-    description="A library for compressing and decompressing 3D geometric meshes and point clouds"
+    license = "Licensed under the Apache License, Version 2.0"
+    description = "A library for compressing and decompressing 3D geometric meshes and point clouds"
 
     scm = {
         "type": "git",
         "url": "https://github.com/google/draco.git",
-        "revision": "master",
+        "revision": "%s" % version,
     }
     
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.BUILD_SHARED_LIBS
+        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
         cmake.configure()
         cmake.build()
 
